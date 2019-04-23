2575bc577a6abc9e56ba30a3ea3da6522f2f2595
Usuario <Usuario@A5-M06>
2019-04-23 17:56:56 -0300

    metodo para validar con JS un usuario

--- prueba de git/web/programa.js
+++ prueba de git/web/programa.js
@@ -3,6 +3,18 @@ class Usuario{
     //string clave;
 
     static entrar(){
-    	cosole.log("helouda")
-    }
+    	console.log("helouda");
+    	let miusuario 	= document.querySelector("#usuario").value;	/*querySelector QUIERE DECIR QUE HACE UNA CONSULTA (.USUARIO) es el ID que se lo llama del */
+    	let miclave 	= document.querySelector("#clave").value;   	/*se puede usar var o let que son familiares pero usar let */
+    	/*USUARIO VALIDO TRUCHO*/
+    	let usuarioValido 	="efygenia";
+    	let claveValida	  	="1234";
+
+    	/*validacion de*/
+    	if (usuarioValido==miusuario && claveValida == miclave){ 
+    		alert("usuario correcto");
+    		}else{ 
+    			alert("usuario incorrecto");
+    		 }
+ }
 }
